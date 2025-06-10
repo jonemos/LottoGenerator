7b033ef31b693ddfd483f36e32fdb209974c9a71
jonemos <jonemos7788@gmail.com>
2025-06-10 11:52:46 +0900

    커밋

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Function to generate random lotto numbers
 def generate_lotto_numbers():
-    # 5개의 번호 조합 생성 11
+    # 5개의 번호 조합 생성 1122
     lotto_numbers = generate_lotto_numbers_all_constraints()
     return lotto_numbers
 
